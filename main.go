bf897edad5a2e95cdbf304c9164a8c0da8bc1d16
alauritz <lauritz.andreas@gmail.com>
2025-09-22 01:29:18 +0200

    Add simple cli structure

--- main.go
+++ main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	if len(os.Args) < 2 {
+		fmt.Println("expected 'start', 'status' or 'delete' subcommands")
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+
+	case "start":
+		startCmd := flag.NewFlagSet("start", flag.ExitOnError)
+		delay := startCmd.Int("delay", 60, "Delay in seconds before shutdown")
+
+		startCmd.Parse(os.Args[2:])
+
+		fmt.Println("Starting with delay:", *delay)
+
+	case "status":
+		fmt.Println("Status: running")
+
+	case "delete":
+		fmt.Println("Shutdown canceled")
+
+	default:
+		fmt.Println("expected 'start', 'status' or 'delete' subcommands")
+		os.Exit(1)
+	}
 }
